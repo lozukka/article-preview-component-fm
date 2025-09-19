d6316ec7af64ab2a523e83b84570ff40b8346f82
lozukka <lotsulotsu@gmail.com>
2025-09-19 15:47:23 +0300

    Add share button functionality and update styles
    Implemented share button logic in script.js to toggle visibility of the share section and writer section. Updated index.html to add an id to the share button. Modified styles.css to initially hide the share section and adjust its z-index.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hei");
+let shareBtn = document.getElementById("share-open");
+let shareSection = document.getElementById("share-section");
+let writerSection = document.getElementById("writer");
+
+shareBtn.addEventListener("click", (event) => {
+  shareSection.style.display = "flex";
+  writerSection.style.display = "none";
+});
